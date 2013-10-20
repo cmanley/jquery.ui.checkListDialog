5d0ed750496c06a54e53741e6e497149ffda995b
Craig Manley <cmanley@xs4all.nl>
2013-10-20 20:00:29 +0200

    Forgot to add these files.

---
+++ i18n/nl.js
@@ -0,0 +1,8 @@
+﻿/*
+jQuery-ui checkListDialog plugin translation file
+Id: nl.js,v 1.1 2013/10/20 17:47:59 cmanley Exp
+*/
+
+(function($) {
+	$.fn.checkListDialog.defaults.toggle_all_text = 'alles (de)selecteren';
+})(jQuery);
